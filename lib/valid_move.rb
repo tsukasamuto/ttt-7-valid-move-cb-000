0762b000ef298e990e49dc81c8d50d50babf1810
Tsukasa Muto <tsukasa610@gmail.com>
2017-09-02 12:30:26 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,7 @@
 # code your #valid_move? method here
-
+def valid_move?(array, index_number)
+  
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(array, index_number)
